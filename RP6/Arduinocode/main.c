a5afddda66a4745d9bc53f837d7087c80fda35f8
Jared Posch <poschjared@hotmail.com>
2017-03-13 14:25:13 +0100

    Commentaar, meer fixes
    Paar fixes die ik tegenkwam. Also, meer commentaar, hoera!

--- RP6/Arduinocode/main.c
+++ RP6/Arduinocode/main.c
@@ -12,6 +12,7 @@ void links();
 void rechts();
 void rechtdoor();
 void stop();
+//Prototypes voor de functies
 
 int main(void)
 {
@@ -72,6 +73,7 @@ int main(void)
 	if (motor[1] > 250) {motor[1] = 250;}
 	if (motor[2] < 0) {motor[2] = 0;}
 	if (motor[2] > 250) {motor[2] = 250;}
+	//Onze snelheid mag niet meer dan 250 zijn, maar ook niet minder dan 0.
 	switch(motor[0]) {
 		case 0:
 		case 12:
@@ -83,6 +85,7 @@ int main(void)
 			snelheid = 0;
 		break;
 	}
+	//Hier slaan we de huidige snelheid op
 }
 
 //TODO doe iets met sensoren
@@ -92,21 +95,26 @@ void rechtdoor () {
 	if (langeafstand == 1) {
 		switch(motor[0]) {
 			case 0:
+			case 4:
+			case 8:
 				motor[0] = 0;
 				motor[1] = snelheid + 25;
 				motor[2] = snelheid + 25;
 			break;
+			//Als de RP6 vooruit rijd of om zijn as draait willen we dat deze (meer) snelheid maakt
 			
 			case 12:
 				motor[0] = 12;
 				motor[1] = snelheid - 25;
 				motor[2] = snelheid - 25;
 			break;
-		}
+			//Als de RP6 achteruit rijd willen we dat deze in snelheid mindert
+			}
 		} else {
 		motor[0] = 0;
 		motor[1] = instelsnelheid;
 		motor[2] = instelsnelheid;
+		//Als de RP6 in de andere mode zit willen we dat deze met de ingestelde snelheid naar achteren rijd
 	}
 	return;
 }
@@ -118,17 +126,22 @@ void achteruit () {
 				motor[1] = snelheid - 25;
 				motor[2] = snelheid - 25;
 			break;
+			//Als de RP6 vooruit rijd willen we dat deze in snelheid mindert
 			
+			case 4:
+			case 8:
 			case 12:
 				motor[0] = 12;
 				motor[1] = snelheid + 25;
 				motor[2] = snelheid + 25;
 			break;
-		}
+			//Als de RP6 achteruit rijd of om zijn as draait willen we dat deze (meer) snelheid maakt
+			}
 		} else {
 		motor[0] = 12;
 		motor[1] = instelsnelheid;
 		motor[2] = instelsnelheid;
+		//Als de RP6 in de andere mode zit willen we dat deze met de ingestelde snelheid naar achteren rijd
 	}
 	return;
 }
@@ -137,25 +150,40 @@ void links () {
 	switch(motor[0]){
 		case 0:
 		case 12:
+		if (snelheid != 0) { 		//Als de RP6 aan het rijden is willen we dat deze een bocht maakt
 			motor[1] = snelheid - 25;
 			motor[2] = snelheid + 25;
-		break;
-		
-		case 4:
-		case 8:
+			} else {
 			motor[0] = 8;
 			motor[1] = 50;
 			motor[2] = 50;
+		}
 		break;
+		//Als de RP6 stilstaat willen we dat deze om zijn as draait
+		
+		case 4:
+		case 8:
+		motor[0] = 8;
+		motor[1] = 50;
+		motor[2] = 50;
+		break;
+		//In dit geval is de RP6 al om zijn as aan het draaien
 	}
 }
-void rechts () {
+void rechts () { //Wordt aangeroepen als we willen dat de Arduino naar rechts gaat
 	switch(motor[0]){
 		case 0:
 		case 12:
-			motor[1] = snelheid + 25;
-			motor[2] = snelheid - 25;
+			if (snelheid != 0) { //Als de RP6 aan het rijden is willen we dat deze een bocht maakt
+				motor[1] = snelheid + 25;
+				motor[2] = snelheid - 25;
+			} else {
+				motor[0] = 4;
+				motor[1] = 50;
+				motor[2] = 50;
+			}
 		break;
+		//Als de RP6 stilstaat willen we dat deze om zijn as draait
 		
 		case 4:
 		case 8:
@@ -163,10 +191,11 @@ void rechts () {
 			motor[1] = 50;
 			motor[2] = 50;
 		break;
+		//In dit geval is de RP6 al om zijn as aan het draaien
 	}
 }
 
-void stop () {
+void stop () { //Wordt aangeroepen als de RP6 compleet stil moet zijn
 	motor[0] = 0;
 	motor[1] = 0;
 	motor[2] = 0;
