af97bbfaa0e7c6c6bcba74c847d8653fb157aab5
Jared Posch <poschjared@hotmail.com>
2017-03-15 12:43:38 +0100

    added command line controls
    The main has also been cleaned by putting the switch statement in
    doStuff()

--- RP6/Arduinocode/main.c
+++ RP6/Arduinocode/main.c
@@ -1,7 +1,7 @@
 #include <avr/io.h>
 int motor[] = {0, 0, 0}; //Richting, motor rechts en motor links
 //Bandenrichting: 0: vooruit. 4: rechts achteruit. 8: links achteruit. 12: achteruit
-int langeafstand; //slaat op of we wel of niet voor lange afstanden bezig zijn
+int langeafstand = 1; //slaat op of we wel of niet voor lange afstanden bezig zijn
 int knop; //Een int met hierin de binaire representatie van welke knoppen zijn ingedrukt
 int lastbuttonpressed= 0; //TODO juiste datatype gebruiken
 int snelheid = 0; //snelheid die het laatste gebruikt is
@@ -12,80 +12,39 @@ void links();
 void rechts();
 void rechtdoor();
 void stop();
+void doStuff();
+void initUSART();
+char uart_getchar(void);
+int charToInt(char);
 //Prototypes voor de functies
 
-int main(void)
-{
-	//TODO get value buttonpressed, longdistance, instelsnelheid
-	if (knop >= 128) {
-		stop();
-	} //Als de noodstop knop ingedrukt is moeten we deze direct uitvoeren
-	else {
-		switch (knop) {
-			case 0: //als we niks indrukken en we zijn niet met de instelsnelheid bezig, stop.
-				if (langeafstand == 0) {
-					stop();
-				}
+int main(void) {
+	initUSART();
+	while(1) {
+		knop = charToInt(uart_getchar());
+		if (knop >= 128) {
+			stop();
+		} //Als de noodstop knop ingedrukt is moeten we deze direct uitvoeren
+		else {
+			doStuff();
+		}
+		if (motor[1] < 0) {motor[1] = 0;}
+		if (motor[1] > 250) {motor[1] = 250;}
+		if (motor[2] < 0) {motor[2] = 0;}
+		if (motor[2] > 250) {motor[2] = 250;}
+		//Onze snelheid mag niet meer dan 250 zijn, maar ook niet minder dan 0.
+		switch(motor[0]) {
+			case 0:
+			case 12:
+				snelheid = (motor[1] + motor[2]) / 2;
 			break;
-
-			case 1: //pijl naar boven
-				rechtdoor();
-			break;
-			
-			case 2: //pijl naar onder
-				achteruit();
-			break;
-
-			case 4: //pijl naar rechts
-				rechts();
-			break;
-
-			case 5: //pijl naar boven & rechts
-				rechtdoor();
-				rechts();
-			break;
-
-			case 6: //pijl naar onder & rechts
-				achteruit();
-				rechts();
-			break;
-			
-			case 8://pijl naar links
-				links();
-			break;
-
-			case 9: //pijl naar boven & links
-				rechtdoor();
-				links();
-			break;
-			
-			case 10: //pijl naar onder & links
-				achteruit();
-				links();
-			break;
-			
-			default:
-			//TODO return error voor invalide combinatie.
-			break;
-		}//TODO correcte waardes toekennen
-	}
-	if (motor[1] < 0) {motor[1] = 0;}
-	if (motor[1] > 250) {motor[1] = 250;}
-	if (motor[2] < 0) {motor[2] = 0;}
-	if (motor[2] > 250) {motor[2] = 250;}
-	//Onze snelheid mag niet meer dan 250 zijn, maar ook niet minder dan 0.
-	switch(motor[0]) {
-		case 0:
-		case 12:
-			snelheid = (motor[1] + motor[2]) / 2;
-		break;
 		
-		case 4:
-		case 8:
-			snelheid = 0;
-		break;
+			case 4:
+			case 8:
+				snelheid = 0;
+			break;
+		}//Hier slaan we de huidige snelheid op
 	}
-	//Hier slaan we de huidige snelheid op
 }
 
 //TODO doe iets met sensoren
@@ -199,4 +158,99 @@ void stop () { //Wordt aangeroepen als de RP6 compleet stil moet zijn
 	motor[0] = 0;
 	motor[1] = 0;
 	motor[2] = 0;
+}
+
+void doStuff() {
+	switch (knop) {
+		case 0: //als we niks indrukken en we zijn niet met de instelsnelheid bezig, stop.
+		if (langeafstand == 0) {
+			stop();
+		}
+		break;
+
+		case 1: //pijl naar boven
+		rechtdoor();
+		break;
+		
+		case 2: //pijl naar onder
+		achteruit();
+		break;
+
+		case 4: //pijl naar rechts
+		rechts();
+		break;
+
+		case 5: //pijl naar boven & rechts
+		rechtdoor();
+		rechts();
+		break;
+
+		case 6: //pijl naar onder & rechts
+		achteruit();
+		rechts();
+		break;
+		
+		case 8://pijl naar links
+		links();
+		break;
+
+		case 9: //pijl naar boven & links
+		rechtdoor();
+		links();
+		break;
+		
+		case 10: //pijl naar onder & links
+		achteruit();
+		links();
+		break;
+		
+		default:
+		//TODO return error voor invalide combinatie.
+		break;
+	}//TODO correcte waardes toekennen
+}
+
+void initUSART() {
+
+	UBRR0H = UBRR_BAUD >> 8;
+	UBRR0L = (uint8_t) UBRR_BAUD;
+	UCSR0A = 0x00;
+	UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);
+	UCSR0B = (1 << TXEN0) | (1 << RXEN0);
+	writeString("usart werkt nog\n\r");
+}
+
+char uart_getchar(void) {
+	loop_until_bit_is_set(UCSR0A, RXC0); /* Wait until data exists. */
+	return UDR0;
+}
+
+int charToInt(char in) {
+	int x;
+	switch(in) {
+		case 'w':
+			x = 1;
+			break;
+		
+		case 's':
+			x = 2;
+			break;
+			
+		case 'a':
+			x = 8;
+			break;
+			
+		case 'd':
+			x = 4;
+			break;
+			
+		case ' ':
+			x = 128;
+			break;
+			
+		default:
+			x = 0;
+			break;
+	}
+	return x;
 }
