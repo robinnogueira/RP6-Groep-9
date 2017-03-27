6bf2b7319621d25032515d0b88f21116469c0873
Jared Posch <poschjared@hotmail.com>
2017-03-27 13:06:23 +0200

    Merge branch 'master' of https://github.com/robinnogueira/RP6-Groep-9 into Bumpers

--- Gui (java)/RP6 GUI/src/rp6/gui/RP6GUI.java
+++ Gui (java)/RP6 GUI/src/rp6/gui/RP6GUI.java
@@ -12,6 +12,8 @@ public class RP6GUI {
     public static void main(String[] args) {
         
         /* open GUI */
+        int test1234 = 127;
+        handmatig.conv(test1234);
         JFrame frame = new GUI(); 
         frame.setVisible(true);  
     }
