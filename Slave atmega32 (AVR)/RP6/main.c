5f6bc7a937dcb0f883d07550939cfe89ba29c7b1
robinnogueira <robin.nogueira@gmail.com>
2017-04-07 13:59:21 +0200

    commends

--- Slave atmega32 (AVR)/RP6/main.c
+++ Slave atmega32 (AVR)/RP6/main.c
@@ -12,10 +12,13 @@
 #include <util/twi.h>
 #include "i2c.h"
 #include "rp6aansluitingen.h"
+
+/*
+	globale variabelen difinen/initiakliseren
+*/
 #define BAUDRATE		38400  
 #define UBRR_BAUD	(((long)F_CPU/((long)16 * BAUDRATE))-1)
 #define resetData()  for(uint8_t i=0;i<20;++i) data[i]=0
-
 #define TRUE 0xFF;
 #define FALSE 0;
 #define aantalsensoren 5
@@ -23,8 +26,12 @@ uint8_t data_ont[20]; //max 20
 volatile uint8_t data_flag = FALSE;
 volatile uint8_t databyte=0x33;
 uint8_t motorbyte[3],i=0;
+int sensoren[aantalsensoren];
+int debounce1,debounce2,ontvangen=0;
 
-
+/*
+	functies definieren 
+*/
 void ontvangData(uint8_t [],uint8_t);
 uint8_t verzendByte();
 void (*ontfunc) (uint8_t[],uint8_t);
@@ -36,61 +43,49 @@ void readaccu();
 void bumpers();
 uint16_t readADC(uint8_t channel);
 
-int sensoren[aantalsensoren];
-int debounce1,debounce2,ontvangen=0;
-/*de interrupt routine van de i2c
-de functie slaaftwi() staat in de library
-*/
+ISR(TWI_vect) {					//I2C interupt
 
-ISR(TWI_vect) {
-
-	slaaftwi();
+	slaaftwi();				//data ontvang functie
 
 }
 
-ISR(TIMER0_COMP_vect)
+ISR(TIMER0_COMP_vect)				//timer interupt functie
 {
-	static volatile int i=0;
+	static volatile int i=0;		//i is een is om verschillende timers te kunnen gebruiken
 	i++;
 	if(i>20)
 	{
 		i=0;
-		if(debounce1<2)debounce1++;
-		if(debounce2<2)debounce2++;
-		ontvangen++;
+		if(debounce1<2)debounce1++;	//debounce linker encoder
+		if(debounce2<2)debounce2++;	//debounce rechter encoder
+		if(ontvangen<2)ontvangen++;	//geen data ontvangen verhogen voor noodstop
 	}
 }
 
-ISR (INT0_vect)
+ISR (INT0_vect)					//externe intterupt van linker encoder
 {
-	if (debounce1>1)
+	if (debounce1>1)			//wanneer er gedebounced is
 	{
-		(motorbyte[0]==0||motorbyte[0]==4)?sensoren[1]++:sensoren[1]--;
-		debounce1=0;
+		(motorbyte[0]==0||motorbyte[0]==4)?sensoren[1]++:sensoren[1]--;		//wanneer de encoder vooruit draait sensoren verhogen, anders aftellen
+		debounce1=0;			//debounce ressetten
 	}
 }
 
-/**
- * External Interrupt 1 ISR
- * (ENCR)
- *
- */
-ISR (INT1_vect)
+ISR (INT1_vect)					//externe intterupt van rechter encoder
 {
-	if (debounce2>1)
+	if (debounce2>1)			//wanneer er gedebounced is
 	{
-		(motorbyte[0]==0||motorbyte[0]==8)?sensoren[2]++:sensoren[2]--;
-			debounce2=0;
+		(motorbyte[0]==0||motorbyte[0]==8)?sensoren[2]++:sensoren[2]--;		//wanneer de encoder vooruit draait sensoren verhogen, anders aftellen
+			debounce2=0;		//debounce ressetten
 		}
 }
 
-int main(void)
+int main(void)					//main functie
 {
-	DDRC=0xBF;
-	encoder_init();
-	initUSART();
-	init_i2c_slave(8);
-	pwm_init();
+	DDRC=0xBF;				//outputs definieren
+	encoder_init();				//encoders initialiseren
+	init_i2c_slave(8);			//I2C slave initialiseren
+	pwm_init();				//Pulse Width Modulation initialiseren
 	
 	/*ontvangData is de functie die uitgevoerd wordt 
 	wanneer een byte via de i2c bus ontvangen wordt
@@ -101,43 +96,23 @@ int main(void)
 	wanneer de slave een byte naar de master verzend*/
 	init_i2c_verzend(verzendByte);
 	
-	sei(); //De slave van i2c werkt met interrupt
+	sei(); //interupts starten
 	
-    /* Replace with your application code */
     while (1) 
     {    
-		readaccu();
-		bumpers();
-		if(sensoren[0]||ontvangen>2)
-			for(int i=0;i<3;i++)
-				motorbyte[i]=0;
-		motoren();
-		if(data_flag) {
-			for (int i =0;i<3;i++)
-				motorbyte[i] = data_ont[i];
-			if(i>2)i=0;
-			//verzendByte(data_ont);
-			/*switch(data_ont[0]) {
-				case 1:PORTC |= SL1;
-				break;
-				case 2:PORTC |= SL2;
-				break;
-				case 3:PORTC |= SL3;
-				break;
-				case 4:PORTC &= ~SL1;
-				break;
-				case 5:PORTC &= ~SL2;
-				break;
-				case 6:PORTC &= ~SL3;
-				break;
-				
-				
-			}*/
-			
-		    data_flag = FALSE;	
-		}
+	readaccu();				//accu spanning uitlezen
+	bumpers();				//bumpers uitlezen
+	if(sensoren[0]||ontvangen>2)		//als er een bumper is ingedrukt of er worden geen gegevens via I2C ontvangen
+		for(int i=0;i<3;i++)		//alle motoren uit zetten
+			motorbyte[i]=0;
+	motoren();				//motoren aansturen
+	if(data_flag) 				//als data is ontvangen
+	{					//motor waardes vullen
+		for (int i =0;i<3;i++)
+			motorbyte[i] = data_ont[i];	
+	    data_flag = FALSE;			//data vlag resseten
+	}
     }
-	
 }
  /*slave heeft data ontvangen van de master
  data[] een array waarin de ontvangen data staat
@@ -146,138 +121,91 @@ int main(void)
 void slaaftwi() {
 	static uint8_t data[40];
 	static uint8_t teller=0;
-	switch(TWSR) {
-		case 0x10:
-		case 0x08:
+	switch(TWSR) {		
+		case 0x60:			//TW_SR_SLA_ACK
+			teller=0;
 		break;
-		
-		case 0x60:
-
-		teller=0;
-
+		case 0x80:			//W_SR_DATA_ACK
+			data[teller++] = TWDR;
 		break;
-		case 0x68:
-
+		case 0xA0:			//TW_SR_STOP
+			ontfunc(data,teller);
+			resetData();
 		break;
-		case 0x80:
-		data[teller++] = TWDR;
-
+		case 0xA8:			//TW_ST_SLA_ACK
+			teller=0;
+			TWDR=verfunc();
 		break;
-		case 0xA0:
-		ontfunc(data,teller);
-		resetData();
-		break;
-		case 0xA8:
-		teller=0;
-		TWDR=verfunc();
-		break;
-		case 0xB8:
-		TWDR=verfunc();
-		sensoren[1]=128;
-		sensoren[2]=128;
-		break;
-		case 0xC0:   //NACK
-		break;
-		case 0xC8:
+		case 0xB8:			//TW_ST_DATA_ACK
+			TWDR=verfunc();
+			sensoren[1]=128;
+			sensoren[2]=128;
 		break;
 	}
-	TWCR |= (1<<TWINT);    // Clear TWINT Flag
+	TWCR |= (1<<TWINT);    			// Clear TWINT Flag
 }
 
 
-void init_i2c_slave(uint8_t ad) {
+void init_i2c_slave(uint8_t ad) {		//I2C initiaseren
 	
-	TWSR = 0;
-	TWBR = ((F_CPU / SCL_frequentie) - 16) / 2;
-	TWCR = (1 << TWIE) | (1 << TWEN) | (1 << TWEA);
-	TWAR = ad<<1;
+	TWSR = 0;				//status register reseten
+	TWBR = ((F_CPU / SCL_frequentie) - 16) / 2;	//baudrate instellen op 9600
+	TWCR = (1 << TWIE) | (1 << TWEN) | (1 << TWEA);	//klaar voor ontvangen instellen
+	TWAR = ad<<1;				//addres instellen
 }
 
-void init_i2c_ontvang( void (*ontvanger) (uint8_t [],uint8_t)) {
+void init_i2c_ontvang( void (*ontvanger) (uint8_t [],uint8_t)) {//I2C ontvang functie instellen (door school geleverd)
 	ontfunc=ontvanger;
 }
 
-void init_i2c_verzend( uint8_t (*verzender) ()) {
+void init_i2c_verzend( uint8_t (*verzender) ()) {//I2C verzend functie instellen (door school geleverd)
 	verfunc=verzender;
 }
 
-void ontvangData(uint8_t data[],uint8_t tel){
-	for(int i=0;i<tel;++i)
-	data_ont[i]=data[i];
-	data_flag = TRUE;
-	writeString("o\n\r");
-	ontvangen=0;
+void ontvangData(uint8_t data[],uint8_t tel){	//I2C data ontvang functie 
+	for(int i=0;i<tel;++i)			//loop om alle data te ontvangen
+		data_ont[i]=data[i];
+	data_flag = TRUE;			//aangeven dat alle data is ontvangen
+	ontvangen=0;				//data ontvangen timer (voor noodstop) resseten 
 }
 
 /* het byte dat de slave verzend naar de master
 in dit voorbeeld een eenvoudige teller
+geleverd door de opleiding
 */
-
 uint8_t verzendByte() {
 	if (i>=aantalsensoren)
 		i=0;
 	return sensoren[i++];
 }
 
-void motoren()
+void motoren()					//funtie die de motoren aanstuurd
 {
-	PORTC =(12&motorbyte[0]);									//motor richting instellen
-	OCR1A = motorbyte[1];										//motor snelheid links
-	OCR1B = motorbyte[2];
-	}
+	PORTC =(12&motorbyte[0]);		//motor richting instellen
+	OCR1A = motorbyte[1];			//motor snelheid links
+	OCR1B = motorbyte[2];			//motor snelheid rechts	
+}
 
-void pwm_init()
+void pwm_init()					//pulse width modulation initialiseren
 {
+	//fast PWM initialiseren op op 1a en 1b
 	TCNT1 = 0;
 	TCCR1A = (0 << WGM10) | (1 << WGM11) | (1 << COM1A1) | (1 << COM1B1);
 	TCCR1B = (1 << WGM13) | (0 << WGM12) | (1 << CS10);
+	ICR1 = 0xFF;			//max instellen
 
-	ICR1 = 0xFF;
-	OCR1A = 0x50;
-	OCR1B = 0x50;
-
-	// make sure to make OC0 pin (pin PB3 for atmega32) as output pin
+	// zorgen dat de motor pinnen als output staan
 	DDRD|=MOTOR_R;
 	DDRD|=MOTOR_L;
 }
 
-void initUSART() {
-
-	UBRRH = UBRR_BAUD >> 8;
-	UBRRL = (uint8_t) UBRR_BAUD;
-	UCSRA = 0x00;
-	UCSRC = (1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0);
-	UCSRB = (1 << TXEN) | (1 << RXEN);
-	writeString("usart werkt nog\n\r");
-}
-
-void writeChar(char ch)
+void encoder_init()				//encoder initialiseren
 {
-	while (!(UCSRA & (1<<UDRE)));
-	UDR = (uint8_t)ch;
-}
-
-void writeString(char *string)
-{
-	while(*string)
-	writeChar(*string++);
-}
-
-void writeInteger(int16_t number, uint8_t base)
-{
-	char buffer[17];
-	itoa(number, &buffer[0], base);
-	writeString(&buffer[0]);
-}
-
-void encoder_init()
-{
-	DDRB |= (SL5 | PWRON);
-	DDRD &= ~(ENC_R | ENC_L);
-	PORTB |= PWRON;
-	PORTD |= (ENC_L | ENC_R);
-	sei();
-	MCUCR |= (1<<ISC00) | (1<<ISC10) | (0<<ISC11) | (0<<ISC01);
+	DDRB |=  PWRON;				//de encoder led als output zetten
+	PORTB |= PWRON;				//de encoder led aan zetten
+	PORTD |= (ENC_L | ENC_R);		//encoders met pullup weerstand instellen
+	//externe interupt instellen op INT0 en INT1
+	MCUCR |= (1<<ISC00) | (1<<ISC10) | (0<<ISC11) | (0<<ISC01);	
 	GICR |= (1<<INT0 | 1<<INT1);
 }
 
@@ -292,10 +220,10 @@ void bumpers (){
 void timer_init()
 {
 	TCCR0 = (1 << WGM12);				//timer op ctc stand zetten (reset on compare)
-	OCR0 = 156;						//aantal ticks voor er wat gebeurt op exact 400 seconden zetten
+	OCR0 = 156;					//aantal ticks voor er wat gebeurt op exact 400 seconden zetten
 	TIMSK = (1 << OCIE0);				//juiste register selecteren
-	sei();								//interupts definieren
-	TCCR0 |= (1 << CS12) | (1 << CS10);//prescale op 1024 zetten
+	sei();						//interupts definieren
+	TCCR0 |= (1 << CS12) | (1 << CS10);		//prescale op 1024 zetten
 }
 
 void readaccu(){
